4dda80ba871e589ed21a85f87e05b0b9a4ae8ebc
nikkeyl <131481562+nikkeyl@users.noreply.github.com>
2024-11-30 00:53:23 +0300

    refactor: stylelint

--- stylelint.config.ts
+++ stylelint.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'stylelint-define-config';
 export default defineConfig({
   extends: ['@archoleat/stylelint-config-extended-scss'],
   rules: {
-    'declaration-empty-line-before': null,
     'selector-class-pattern': null,
   },
 });
