167a62a9516db52c2fa8bde0a88f2e611d175962
nikkeyl <131481562+nikkeyl@users.noreply.github.com>
2025-03-22 00:16:15 +0300

    refactor: small changes

--- environments.d.ts
+++
@@ -1,8 +0,0 @@
-declare namespace NodeJS {
-  export interface ProcessEnv {
-    NEXT_PUBLIC_AUTHOR_NAME: string;
-    NEXT_PUBLIC_DEFAULT_DESCRIPTION: string;
-    NEXT_PUBLIC_DEFAULT_TITLE: string;
-    NEXT_PUBLIC_DEFAULT_URL: string;
-  }
-}
